678955e28f21a53c0f5a2e137b5b09231e3bf2bd
CiaranOC <129303811+CiaranO-C@users.noreply.github.com>
2024-11-25 14:39:35 +0000

    remove file and entry types in favour of CSV row and file types

--- src/file/type.ts
+++ src/file/type.ts
@@ -4,16 +4,8 @@ interface CSVRow {
   [key: string]: string;
 }
 
-interface FileItem {
-  [key: string]: string;
-}
-
-interface EntryItem {
-  [key: string]: string | boolean | number;
-}
-
-type FileData = Array<FileItem>;
+type CSVFile = Array<CSVRow>;
 
 type DataCategory = "buyer" | "sale" | "item";
 
-export type { CSVRow, FileData, FileItem, EntryItem, DataCategory };
+export type { CSVRow, CSVFile, DataCategory };
