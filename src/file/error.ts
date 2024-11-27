014b9df2555246518558b61bdcfa99735194c45c
CiaranOC <129303811+CiaranO-C@users.noreply.github.com>
2024-11-27 22:34:10 +0000

    amend error constructor to include extension property

--- src/file/error.ts
+++ src/file/error.ts
@@ -1,12 +1,13 @@
 import { GraphQLError } from "graphql";
 
 class UploadError extends GraphQLError {
-  code: string;
-
   constructor(message: string) {
-    super(message);
-    this.code = "UPLOAD_ERROR";
+    super(message, {
+      extensions: {
+        code: "UPLOAD_ERROR",
+      },
+    });
   }
 }
 
-export { UploadError }
+export { UploadError };
