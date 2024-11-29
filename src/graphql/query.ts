bb058ab4f8016e45b846b0cf5573a3234c2789b9
CiaranOC <129303811+CiaranO-C@users.noreply.github.com>
2024-11-29 10:17:09 +0000

    import fields directly, remove logic

--- src/graphql/query.ts
+++ src/graphql/query.ts
@@ -1,32 +1,10 @@
-import {
-  GraphQLBoolean,
-  GraphQLInt,
-  GraphQLObjectType,
-  GraphQLString,
-} from "graphql";
-
-const BuyerType = new GraphQLObjectType({
-  name: "Buyer",
-  fields: {
-    id: { type: GraphQLInt },
-    username: { type: GraphQLString },
-    city: { type: GraphQLString },
-    country: { type: GraphQLString },
-  },
-});
+import { GraphQLObjectType } from "graphql";
+import { salesAnalytics } from "../sales/query";
 
 const RootQuery = new GraphQLObjectType({
   name: "RootQueryType",
   fields: {
-    getBuyer: {
-      type: BuyerType,
-      args: {
-        id: { type: GraphQLInt },
-      },
-      resolve: (_, { id }) => {
-        console.log("Found buyer");
-      },
-    },
+    salesAnalytics,
   },
 });
 
