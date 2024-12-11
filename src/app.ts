f1619735afeaca0e7a13b33b658af0a851f43f11
CiaranOC <129303811+CiaranO-C@users.noreply.github.com>
2024-12-11 20:12:07 +0000

    create dedicated REST upload endpoint

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import { configDotenv } from "dotenv";
 configDotenv();
-import e from "express";
+import e, { Request } from "express";
 import cors from "cors";
 import { createHandler } from "graphql-http/lib/use/express";
 import schema from "./graphql/schema";
+
+import { uploadRouter } from "./upload/uploadRouter";
 
 const app = e();
 const PORT = process.env.PORT || 5500;
@@ -16,7 +18,24 @@ const options: cors.CorsOptions = {
 
 app.use(cors(options));
 app.use(e.json({ limit: "50mb" }));
+app.use(e.urlencoded({ extended: true }));
 
-app.all("/graphql", createHandler({ schema }));
+app.use("/upload", uploadRouter);
+app.use((req, res, next) => {
+  console.log(req.body); // logs fine here
+  next();
+});
+app.all(
+  "/graphql",
+  createHandler({
+    schema,
+    context: (req, args) => {
+      const { userId } = req.raw.body.variables;
+      return { userId };
+    },
+  }),
+);
+
+
 
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
