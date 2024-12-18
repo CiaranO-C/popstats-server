f622636e36bacd40766be4eb09a4463018456bf1
CiaranOC <129303811+CiaranO-C@users.noreply.github.com>
2024-12-18 23:41:53 +0000

    write group months function and monthly average function

--- src/statistics/sales/resolver.ts
+++ src/statistics/sales/resolver.ts
@@ -1,5 +1,3 @@
-import prisma from "../../../config/prisma";
-import { getDateRange } from "../files/db";
 import { aggregateSales, countSales, groupSales } from "./db";
 
 async function getSaleCount(parent, args, context) {
@@ -42,4 +40,72 @@ async function getTotalSales(parent, args, context) {
   return { sales, revenue: _sum?.total };
 }
 
-export { getSaleCount, getSalesByDate, getTotalSales };
+async function getAverageSaleByDate(parent, args, context) {
+  const { userId }: { userId: string } = context;
+  const dailyGroup = await groupSales({
+    by: ["dateOfSale"],
+    where: { userId },
+    _avg: {
+      total: true,
+    },
+    orderBy: { dateOfSale: "asc" },
+  });
+  const monthlyGroup = await groupSales({
+    by: ["dateOfSale"],
+    where: {
+      userId,
+    },
+    orderBy: { dateOfSale: "asc" },
+    _sum: {
+      total: true,
+    },
+  });
+  const monthly = calcMonthlyAverage(monthlyGroup);
+
+  return {
+    daily: dailyGroup.map(({ _avg, dateOfSale }) => {
+      console.log(dateOfSale);
+      
+      return {
+      average: _avg?.total?.toFixed(2),
+      date: dateOfSale.toISOString(),
+    }}),
+    monthly,
+  };
+}
+
+function groupMonths(data) {
+  const groupedData = {};
+  for (let i = 0; i < data.length; i++) {
+    const { _sum, dateOfSale } = data[i];
+    const total = parseFloat(_sum.total);
+    const month = new Date(dateOfSale).getUTCMonth();
+    if (groupedData.hasOwnProperty(month)) {
+      groupedData[month].total += total;
+      groupedData[month].length++;
+      continue;
+    }
+    groupedData[month] = { total, length: 1 };
+  }
+
+  return groupedData;
+}
+
+
+
+function calcMonthlyAverage(data) {
+  const monthlyData: { [field: string]: { total: number; length: number } } =
+    groupMonths(data);
+  const monthArr = Object.entries(monthlyData);
+  const averageArr = monthArr.map((month) => {
+    const average = month[1].total / month[1].length;
+    return {
+      average: average.toFixed(2),
+      month: month[0],
+    };
+  });
+
+  return averageArr;
+}
+
+export { getSaleCount, getSalesByDate, getTotalSales, getAverageSaleByDate };
