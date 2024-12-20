520f30d9edf49a09a6042563695d43b5c49cbbe7
CiaranOC <129303811+CiaranO-C@users.noreply.github.com>
2024-12-20 13:18:26 +0000

    remove sales queries

--- src/statistics/query.ts
+++ src/statistics/query.ts
@@ -5,84 +5,20 @@ import {
   GraphQLObjectType,
   GraphQLString,
 } from "graphql";
-import { getAverageSaleValue, getMostPopularCategory } from "./resolvers";
+import { getMostPopularCategory } from "./resolvers";
 import {
-  getAverageSaleByDate,
-  getSalesByDate,
-  getTotalSales,
-} from "./sales/resolver";
-import { GraphQLDateTime } from "graphql-scalars";
+  averageSales,
+  salesByDate,
+  totalSales,
+} from "./sales/query";
 
 const SalesAnalyticsType = new GraphQLObjectType({
   name: "SalesAnalytics",
   fields: {
-    salesByDate: {
-      type: new GraphQLList(
-        new GraphQLObjectType({
-          name: "SalesByDate",
-          fields: {
-            total: { type: GraphQLFloat },
-            date: { type: GraphQLDateTime },
-          },
-        }),
-      ),
-      resolve: getSalesByDate,
-    },
-    totalSales: {
-      type: new GraphQLObjectType({
-        name: "TotalSales",
-        fields: {
-          sales: { type: GraphQLInt },
-          revenue: { type: GraphQLFloat },
-        },
-      }),
-      resolve: getTotalSales,
-    },
-    averageSales: {
-      type: new GraphQLObjectType({
-        name: "AverageSales",
-        fields: {
-          byDate: {
-            type: new GraphQLObjectType({
-              name: "AverageSalesByDate",
-              fields: {
-                monthly: {
-                  type: new GraphQLList(
-                    new GraphQLObjectType({
-                      name: "AverageSalesMonthly",
-                      fields: {
-                        average: { type: GraphQLFloat },
-                        month: { type: GraphQLString },
-                      },
-                    }),
-                  ),
-                },
-                daily: {
-                  type: new GraphQLList(
-                    new GraphQLObjectType({
-                      name: "AverageSalesDaily",
-                      fields: {
-                        average: { type: GraphQLFloat },
-                        date: { type: GraphQLString },
-                      },
-                    }),
-                  ),
-                },
-              },
-            }),
-            resolve: getAverageSaleByDate,
-          },
-          byTime: {
-            type: GraphQLFloat,
-            resolve: () => {
-              /*get average sales by time of day*/
-            },
-          },
-        },
-      }),
-      resolve: () => ({}),
-    },
-    averageSaleValue: { type: GraphQLFloat, resolve: getAverageSaleValue },
+    salesByDate,
+    totalSales,
+    averageSales,
+    //averageSaleValue: { type: GraphQLFloat, resolve: getAverageSaleValue },
     totalRevenue: { type: GraphQLFloat },
     mostPopularCategory: {
       type: GraphQLString,
