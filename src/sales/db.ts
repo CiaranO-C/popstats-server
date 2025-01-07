07f1adb4c2c6f7093b241c9bdd01ffcba55bfb85
CiaranOC <129303811+CiaranO-C@users.noreply.github.com>
2025-01-07 22:58:31 +0000

    write raw queries for count fields

--- src/sales/db.ts
+++ src/sales/db.ts
@@ -6,6 +6,41 @@ async function countSales(condition: Prisma.SaleWhereInput): Promise<number> {
   return count;
 }
 
+async function countSalesByDate(userId: string) {
+  const count = await prisma.$queryRaw<
+    { date: Date; sales: bigint; refunds: bigint; bundles: bigint }[]
+  >`
+  SELECT
+    date_of_sale AS date,
+    COUNT(*) AS sales,
+    COUNT(CASE WHEN buyer_refund > 0 THEN 1 ELSE NULL END) AS refunds,
+    COUNT(CASE WHEN bundle THEN 1 ELSE NULL END) AS bundles
+  FROM sales
+  WHERE user_id = ${userId}
+  GROUP BY date_of_sale
+  ORDER BY date_of_sale ASC;`;
+
+  return count;
+}
+
+async function countSaleItemsByDate(userId: string) {
+  const count = await prisma.$queryRaw<
+    { date: Date; items: bigint; boosted: bigint }[]
+  >`
+  SELECT 
+    date_of_sale AS date,
+    COUNT(items) AS items,
+    COUNT(CASE WHEN boosting_fee > 0 THEN 1 ELSE NULL END) AS boosted
+  FROM sales
+  LEFT JOIN items
+  ON sales.id = sale_id
+  WHERE sales.user_id = ${userId}
+  GROUP BY date_of_sale
+  ORDER BY date_of_sale ASC;`;
+
+  return count;
+}
+
 async function groupSales(
   args: Prisma.SaleGroupByArgs & {
     orderBy: Prisma.SaleGroupByArgs["orderBy"];
@@ -20,4 +55,16 @@ async function aggregateSales(args: Prisma.SaleAggregateArgs) {
   return res;
 }
 
-export { countSales, groupSales, aggregateSales };
+async function findSales(args: Prisma.SaleFindManyArgs) {
+  const res = await prisma.sale.findMany(args);
+  return res;
+}
+
+export {
+  countSales,
+  countSalesByDate,
+  countSaleItemsByDate,
+  groupSales,
+  aggregateSales,
+  findSales,
+};
