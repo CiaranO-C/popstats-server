f39d92b90dcc9ccbecaa350040358ca1cfc43e52
CiaranOC <129303811+CiaranO-C@users.noreply.github.com>
2025-01-09 13:13:20 +0000

    create shared utils directory

---
+++ src/utils/convert.ts
@@ -0,0 +1,17 @@
+function convertBigInts(
+  dataArr: { [field: string]: Date | bigint }[],
+  keysToIgnore: string[],
+) {
+  const converted = dataArr.map((row) => {
+    const rowArr = Object.entries(row);
+    const converted = rowArr.map(([key, value]) => {
+      if (keysToIgnore.includes(key)) return [key, value];
+      return [key, Number(value)];
+    });
+
+    return Object.fromEntries(converted);
+  });
+  return converted;
+}
+
+export { convertBigInts };
