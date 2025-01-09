5de8d3ab9a50f7f320d0a9c25a7099e58a7c92d7
CiaranOC <129303811+CiaranO-C@users.noreply.github.com>
2025-01-09 13:31:53 +0000

    write buyersByCountry sql query

--- src/buyers/db.ts
+++ src/buyers/db.ts
@@ -8,9 +8,26 @@ async function countBuyers(args: Prisma.UserBuyerCountArgs) {
 }
 
 async function findManyBuyers(args: Prisma.UserBuyerFindManyArgs) {
-    const buyers = await prisma.userBuyer.findMany(args)
+  const buyers = await prisma.userBuyer.findMany(args);
 
-    return buyers
+  return buyers;
 }
 
-export { countBuyers, findManyBuyers };
+async function buyersByCountry(userId: string) {
+  const buyers: { buyers: bigint; country: string }[] = await prisma.$queryRaw`
+    SELECT
+      COUNT(buyer_locations.username) AS buyers,
+      country
+    FROM user_buyers
+    JOIN buyers
+      ON user_buyers.buyer_id = buyers.username
+    JOIN buyer_locations
+      ON buyers.username = buyer_locations.username
+    GROUP BY country, user_buyers.user_id
+      HAVING user_buyers.user_id = ${userId};
+    `;
+
+  return buyers;
+}
+
+export { countBuyers, findManyBuyers, buyersByCountry };
