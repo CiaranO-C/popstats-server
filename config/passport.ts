8c19b4b99dbbc48bd271ce917f841e51b9d30a8c
CiaranOC <129303811+CiaranO-C@users.noreply.github.com>
2025-03-19 13:44:53 +0000

    install and config graphQL-passport, local strat

--- config/passport.ts
+++ config/passport.ts
@@ -3,8 +3,34 @@ import { Strategy as LocalStrategy } from "passport-local";
 import bcrypt from "bcryptjs";
 import { User } from "@prisma/client";
 import prisma from "./prisma";
+import { GraphQLLocalStrategy } from "graphql-passport";
 
 passport.use(
+  new GraphQLLocalStrategy(async (username: string, password: string, done) => {
+    try {
+      const user: User | null = await prisma.user.findUnique({
+        where: { username },
+      });
+
+      if (!user) {
+        return done(null, false, "Username does not exist");
+      }
+
+      const isMatch: boolean = await bcrypt.compare(password, user.password);
+
+      if (!isMatch) {
+        return done(null, false, "Incorrect password");
+      }
+
+      //Correct details entered
+      return done(null, user);
+    } catch (error) {
+      return done(error);
+    }
+  }),
+);
+
+/*passport.use(
   "login",
   new LocalStrategy(
     async (username: string, password: string, done: Function) => {
@@ -31,6 +57,6 @@ passport.use(
       }
     },
   ),
-);
+);*/
 
-export { passport }
+export { passport };
